771c21dc9bda86790d81cb4c7628eaf294afc960
zhigao <zhigao@google.com>
2019-06-24 22:02:16 +0800

    k8s_deployment

---
+++ tools/kubernetes/html/daemon_files/rate.php
@@ -0,0 +1,6 @@
+PDRTJS_8453675.nero_up = 60;
+PDRTJS_8453675.nero_dn = 89;		
+PDRTJS_settings_8453675= { "type" : "nero" , "size" : "sml" , "star_color" : "hand" , "custom_star" : "" , "font_size" : "12px" , "font_line_height" : "20px" , "font_color" : "333333" , "font_align" : "center" , "font_position" : "top" , "font_family" : "verdana" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate this page:" , "text_1_star" : "Awful" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate this page:" , "text_rate_down" : "Rate this page:" , "popup" : "off"  };
+PDRTJS_8453675.init();		
+PDRTJS_8453675.token='27bcc476d79238bd9d2225a9e6d14157';
+/*8453675,,engine/reference/commandline/dockerd.md,1899832133,60-89*/
