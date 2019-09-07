9d4b05b6b3ef64c651ffbdb65007da8a8cd4a110
zhigao <zhigao@google.com>
2019-09-08 01:54:25 +0800

    python1-2

--- tools/python/code/hello.py
+++ tools/python/code/hello.py
@@ -1,9 +1,34 @@
-print("hello world")
-a=5
-b=6
-#a,b=b,a
-# print("a =",a)
-# print("b =",b)
-# help("__main__")
-id(a)
-id(b)
+# print("hello world")
+#1.
+# a=5
+# b=6
+# #a,b=b,a
+# # print("a =",a)
+# # print("b =",b)
+# # help("__main__")
+# print(id(a))
+# print(a > b)
+# 2.
+# x=123.455
+# b=123.45500001      
+# print(round(x))
+# print(round(x,2))
+# print(round(b,2))    #因为计算机会先进行二进制转换在进行四舍五入
+# print(round(x,-1))   #小数往左
+# 3.
+# x = -100
+# print(abs(x))  #绝对值
+# 4.
+# s = input('请输入：')
+# print('你输入:',s)
+# 5.
+# print(1,2,3,sep='#')
+# print("hello world",sep="##")
+# print(5,6,7,end='###\n')
+# 6.
+# hour = input("num:")
+# print(hour * 60)       #字符串是可以相乘的
+# hour = int(hour)
+# print(hour * 60)
+if 100:
+	print("Ture")
