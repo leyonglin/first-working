717eecaaf4cc3f13806bdb58822f89b16a958bd3
zhigao <zhigao@google.com>
2019-09-09 01:55:14 +0800

    list

--- tools/python/code/hello.py
+++ tools/python/code/hello.py
@@ -32,8 +32,90 @@
 # print(hour * 60)
 # if 100:
 # 	print("Ture")
-s = 'aaa\nbbb'
-print(s)
-s1 = '''aaa   #三引号可以自动折行
-bbb'''
-print(s1)
+# s = 'aaa\nbbb'
+# print(s)
+# s1 = '''aaa   #三引号可以自动折行
+# bbb'''
+# print(s1)
+# fmt = "name: %s, age: %d"
+# s= fmt % ('lly', 35)
+# print(s)
+# pi=3.141592535897932
+# print("-+%07.2f"  %  pi)
+# s = int(input('请输入整数：'))
+# i = 1
+# j = 1 
+# while j <= s:
+# 	j +=1
+# 	# i += 1/2**j
+# 	print(1/2**j)
+# i = int(input('请输入整数：'))
+# j = 0
+# while j < i:
+#     print(' ' * j + '*' * (i-j))
+#     j += 1
+
+# for x in range(4,0,-1):
+# 	print(x)
+
+# i=6
+# for x in range(1,i):          #这里的range只被调用一次
+# 	print('x=',x,'i=',i)
+# 	i -=1
+
+# a = int(input('num:'))
+# for x in range(1,a+1):        #该range只被掉用一次
+# 	print('x=',x)
+# 	for y in range(1,a+1):
+# 		print(y,end=" ")
+# 	print()
+# 	x+2
+
+# a=input('请输入')
+# b=input('请输入')
+# c=input('请输入')
+# L=[]
+# 1.
+# L += a
+# L += b
+# L += c
+# 2.
+# L += [a]
+# L += [b]
+# L += [c]
+# 3.
+# L=[a,b,c]
+# print(L)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
