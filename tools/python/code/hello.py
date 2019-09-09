96fc962b9321862d718fc6952a1d6a3c886e824b
zhigao <zhigao@google.com>
2019-09-10 00:02:59 +0800

    tuple&&set

--- tools/python/code/hello.py
+++ tools/python/code/hello.py
@@ -87,6 +87,78 @@
 # L=[a,b,c]
 # print(L)
 
+# L = [1, 2, 3, 4]
+# print(L[1::2])
+# L[1::2] = "ab"
+# #L[1::2] = "abc"    这个会报错，左右个数不一致
+# print(L) 
+
+# L = [1, 2, 3, 4]
+# L2=L
+# print(L,L2)
+# L2[1] = 1000
+# print(L,L2)
+
+# import copy
+# L=[3.1, 3.2]
+# L1=[1,2,L]
+# # L2=L1.copy()        #浅拷贝
+# L2=copy.deepcopy(L1)  #深拷贝
+# print(L,L1,L2)
+# L2[2][0]=3.14
+# print(L,L1,L2)
+
+# begin=int(input('num1:'))
+# end=int(input('num2:'))
+# print([i for i in range(begin,end) if i % 2 ==0])
+
+# s='1,2,3'
+# print(s.split(','))
+# print([int(i) for i in s.split(',')])
+
+# d1=dict((['name', 'lin'], 'ab'))
+# print(d1)
+# d2=dict(name='lin', age=15)
+# print(d2)
+
+# d={1:'one', 2:'two',3:'three'}
+# # for i in d.values():
+# # for i in d.keys():
+# for i,j in d.items():
+# 	print(i,j)
+
+# L=[]
+# a=0
+# b=1
+# while len(L) < 40:
+# 	L.append(b)
+# 	a, b=b, a+b
+# 	# c=a+b
+# 	# a=b
+# 	# b=c
+# print(L)
+
+# L=[1,1]
+# while len(L) < 40:
+# 	L.append(L[-1]+L[-2])
+# print(L)
+print(set({1:1, 2:2}.items()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
