349a2e6acce29d64556433c721592534e6bbf2de
master <master@qq.com>
2019-09-14 13:33:04 +0800

    class

--- tools/python/code/oop.py
+++ tools/python/code/oop.py
@@ -2,8 +2,9 @@
 class Dog:                                                   #创建类
 	def eat(self, food):                                     #创建实例方法(共性),self被赋值为实例对象
 		print(self.color, '的', self.kinds, '正在吃', food)   #可以调用实例属性
-		self.food=food                                       #保存对象自己的数据(个性)，该属性由dog1.eat('骨头') 执行后赋值的
+		self.food=food                                       #保存对象自己的数据(个性)，该属性由dog1.eat('骨头') 执行后赋值的，和dog1.kinds='二哈'作用是一样的
 	def show_food(self):
+		# print(self.color, '的', self.kinds, '正在吃1', food) #会失败，显示food未定义  
 		print(self.color, '的', self.kinds, '上次吃的是', self.food)
 
 dog1=Dog()                                                   #创建实例对象
