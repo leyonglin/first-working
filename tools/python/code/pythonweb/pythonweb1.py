632411855c6c8fe3ef7fb421a19a754197e1c8c9
master <master@qq.com>
2019-10-06 23:47:04 +0800

    login_judgment

--- tools/python/code/pythonweb/pythonweb1.py
+++ tools/python/code/pythonweb/pythonweb1.py
@@ -428,33 +428,78 @@ db = SQLAlchemy(app)
 #     return render_template('02-template.html',teachers=teachers)
 
 
-#存cookie
-@app.route('/01-serCookie')
-def serCookie():
-    resp = make_response('添加cookie成功')
-    #通过resp保存cookie的值到客户端浏览器中
-    resp.set_cookie('uname','jack',60*60*24*365)
-    return resp
-#取cookie
-@app.route('/02-getCookie')
-def getCookie():
-    print(request.cookies)
-    return "获取cookies成功"
+# #存cookie
+# @app.route('/01-serCookie')
+# def serCookie():
+#     resp = make_response('添加cookie成功')
+#     #通过resp保存cookie的值到客户端浏览器中
+#     resp.set_cookie('uname','jack',60*60*24*365)
+#     return resp
+# #取cookie
+# @app.route('/02-getCookie')
+# def getCookie():
+#     print(request.cookies)
+#     return "获取cookies成功"
 
 
-#存session
-@app.route('/03-setSession')
-def setSession():
-    session['uname'] = 'MrWang'
-    return "Set Session Success"
-#取session
-@app.route('/04-getSession')
-def getSession():
-    uname = session.get('uname','')
-    if uname:
-        return "用户名为"+uname
-    else:
-        return "没找到session信息"
+# #存session
+# @app.route('/03-setSession')
+# def setSession():
+#     session['uname'] = 'MrWang'
+#     return "Set Session Success"
+# #取session
+# @app.route('/04-getSession')
+# def getSession():
+#     uname = session.get('uname','')
+#     if uname:
+#         return "用户名为"+uname
+#     else:
+#         return "没找到session信息"
+
+
+#a登录状态判断
+@app.route('/')
+def index():
+	return "欢迎来到首页"
+
+@app.route('/01-template',methods=['GET','POST'])
+def login():
+	if request.method == "GET":
+		#判断是否已经在登录状态上，判断session中是否有uname的值
+		if 'uname' in session:
+			#已经登录
+			return	redirect('/')
+		else:
+			#没有登录，继续向下判断cookie
+			if 'uname' in request.cookies:
+				#曾静记住过密码，取出值保存进session
+				uname = request.cookies.get('uname')
+				session['uname'] = uname
+				return redirect('/')
+			else:
+				#之前没有登陆过
+				return render_template('01-template.html')
+	else:
+		#先处理登录，登录成功则保存进session，否则回到登录页
+		uname = request.form.get('uname')
+		upwd = request.form.get('upwd')
+		print(uname,upwd)
+		if uname == 'admin' and upwd == 'admin':
+			#什么重定向到首页的对象
+			resp = redirect('/')
+			#登录成功：现将数据保存进session
+			session['uname'] = uname
+			if 'isSaved' in request.form:
+				#需要记住密码，将信息保存进cookie
+				resp.set_cookie('uname',uname,60*60*24*365)
+			return resp
+		else:
+			#登录失败
+			return redirect('/01-template')
+
+
+
+
 
 
 
