c551cf8c63af0b9a22316c0283232a1431aacd0f
master <master@qq.com>
2019-10-08 00:52:12 +0800

    ajax3_project

--- tools/python/code/AJAX/static/common.js
+++
@@ -1,7 +0,0 @@
-function getXhr(){
-    if(window.XMLHttpRequest){ 
-        return new XMLHttpRequest();              
-    }else{
-        return new  ActiveXObject("Microsoft.XMLHTTP")
-    }
-}
--- tools/python/code/AJAX/static/js/common.js
+++ tools/python/code/AJAX/static/js/common.js
@@ -1,1 +1,17 @@
-common.js
+function getXhr(){
+    if(window.XMLHttpRequest){ 
+        return new XMLHttpRequest();              
+    }else{
+        return new  ActiveXObject("Microsoft.XMLHTTP")
+    }
+}
+
+function createXhr(){
+	var xhr=null;
+	if(window.XMLHttpRequest){
+		xhr = new XMLHttpRequest();
+	}else{
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return xhr;
+}
