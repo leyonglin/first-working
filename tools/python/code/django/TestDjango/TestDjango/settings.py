97c21e715630f5c7671cd95319451f17ae3d9def
zhigao <zhigao@google.com>
2019-10-12 19:47:16 +0800

    django_model

--- tools/python/code/django/TestDjango/TestDjango/settings.py
+++ tools/python/code/django/TestDjango/TestDjango/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webdb',
+        'USER': 'root',
+        'PASSWORD': 123456,
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
+
+
 
 
 # Password validation
--- tools/python/code/django/TestDjango/index/models.py
+++ tools/python/code/django/TestDjango/index/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+#在数据库先创建好库：create database webdb default charset utf8 collate utf8_general_ci;
+#创建一个实体类(表) - Publisher(出版社)
+#1.name:出版社名称(varcher(30))
+#2.address:出版社地址(varchar(200))
+#3.city:出版社所在城市(varchar(50))
+#4.country:出版社城市(varchar(50))
+#5.website:出版社的网址(varchar(20))
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+#追加两个表,如果需要修改/删除表，在实体类这里修改/删除
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=50,null=True)
+#添加一个字段isActive表示用户激活状态，数据类型为BooleanField(),新增字段要么允许为空，要么设置默认值，不然会冲突
+    isActive = models.BooleanField(default=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    publicate_date = models.DateField()
