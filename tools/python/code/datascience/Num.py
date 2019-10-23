7a9d9e1d6aca17473e9471584188a654686c1198
master <master@qq.com>
2019-10-23 13:04:21 +0800

    numpy

---
+++ tools/python/code/datascience/Num.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+import datetime as dt
+import numpy as np
+n = 100000
+start = dt.datatime.now()
+A,B = [],[]
+for i in range(n):
+	A.append(i**2)
+	B.append(i**3)
+C=[]
+for a,b in zip(A,B):
+	C.append(a+b)
+print((dt.datatime.now()-start).microseconds)
+start = dt.datatime.now()
+A,B = np.arange(n)**2,np.arange(n)**3
+C=A+B
+print((dt.datatime.now()-start).microseconds)
